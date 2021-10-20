6a936314c75c5ea122f2d8fc2c26660b6a7e613c
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-10-20 21:41:18 +0200

    implement basic HTTP_INTERCEPTOR

---
+++ src/app/core/add-header.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AddHeaderInterceptor implements HttpInterceptor {
+    constructor() {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log(`AddHeaderInterceptor - ${req.url}`);
+    
+        const headers = req.headers
+            .set('Content-Type', 'application/json');
+        const authReq = req.clone({ headers });
+        return next.handle(authReq);
+    }
+}
